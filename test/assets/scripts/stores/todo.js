62ec816292b6bd84ecdaf17902d9f738f83231ed
Callum Fraser <email@callumjfraser.com>
2017-01-08 18:58:09 +0000

    Add test for getTodos function to todo store

--- test/assets/scripts/stores/todo.js
+++ test/assets/scripts/stores/todo.js
@@ -19,6 +19,26 @@ describe('Todo store', function () {
 		}
 	});
 
+	it('should return current list of todos',function () {
+		rollback = todoStore.__set__('_todos', [{
+			id: 12345,
+			text: '0one2three4five6seven8nine'
+		}]);
+
+		var todos = todoStore.getTodos();
+		todos[0].id.should.equal(12345);
+		todos[0].text.should.equal('0one2three4five6seven8nine');
+
+		rollback = todoStore.__set__('_todos', [{
+			id: 54321,
+			text: '9eight7six5four3two1zero'
+		}]);
+
+		todos = todoStore.getTodos();
+		todos[0].id.should.equal(54321);
+		todos[0].text.should.equal('9eight7six5four3two1zero');
+	});
+
 	it('should handle todo create action', function () {
 		const handleDispatch = todoStore.__get__('handleDispatch');
 
@@ -29,7 +49,7 @@ describe('Todo store', function () {
 			}
 		});
 
-		const todos = todoStore.getTodos();
+		const todos = todoStore.__get__('_todos');
 		todos[0].text.should.equal('testing this shizzle');
 	});
 
@@ -45,7 +65,7 @@ describe('Todo store', function () {
 			data: 1
 		});
 
-		const todos = todoStore.getTodos();
+		const todos = todoStore.__get__('_todos');
 		todos.length.should.equal(0);
 	});
 
@@ -64,7 +84,7 @@ describe('Todo store', function () {
 			}
 		});
 
-		const todos = todoStore.getTodos();
+		const todos = todoStore.__get__('_todos');
 		todos[0].text.should.equal('updated text');
 	});
 });
