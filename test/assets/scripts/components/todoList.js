5fed948555f22260d057049c56abdc1152ced35d
Callum Fraser <email@callumjfraser.com>
2017-01-15 22:34:18 +0000

    Add basic todoList component test.

---
+++ test/assets/scripts/components/todoList.js
@@ -0,0 +1,20 @@
+require('chai').should();
+const React = require('react');
+const ReactDOM = require('react-dom');
+const TestUtils = require('react-addons-test-utils');
+
+const TodoList = require('../../../../assets/scripts/components/todoList');
+
+describe('TodoList', function () {
+	const renderComponent = function () {
+		const component = TestUtils.renderIntoDocument(<TodoList />);
+		return ReactDOM.findDOMNode(component);
+	};
+
+	it('should render with filled name input and remove button', function () {
+		const todoList = renderComponent();
+
+		const title = todoList.querySelectorAll('h2')[0];
+		title[0].textContent.should.equal('Todos');
+	});
+});
